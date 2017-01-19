ec18b63acbe271d6a9129dde2552023388969951
dmzhg <dmitry.zhgun@onegolabs.com>
2017-01-19 17:51:30 +0300

    Added preloader to init settings and show JavaFX messages

---
+++ src/main/java/com/onegolabs/wamanager/MyPreLoader.java
@@ -0,0 +1,22 @@
+package com.onegolabs.wamanager;
+
+import javafx.application.Preloader;
+import javafx.stage.Stage;
+
+/**
+ * @author dmzhg
+ */
+public class MyPreLoader extends Preloader {
+
+	private static Configuration settings;
+
+	public static Configuration getConfiguration() {
+		return settings;
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		settings = new Configuration();
+		settings.init();
+	}
+}
