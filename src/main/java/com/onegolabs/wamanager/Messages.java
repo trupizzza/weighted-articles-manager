bb7858dffc07d3792682c5f073ae87721a2b36be
dmzhg <dmitry.zhgun@onegolabs.com>
2017-01-19 15:31:45 +0300

    Messages separated to accessible class

---
+++ src/main/java/com/onegolabs/wamanager/Messages.java
@@ -0,0 +1,19 @@
+package com.onegolabs.wamanager;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * @author dmzhg
+ */
+public class Messages {
+
+	public static ResourceBundle r = ResourceBundle.getBundle("language", new Locale("ru"));
+
+	private Messages() {
+	}
+
+	public static String getString(String key) {
+		return r.getString(key);
+	}
+}
