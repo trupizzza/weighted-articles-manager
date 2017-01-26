759d47ea3b424ce352493bc1e7917a6af64cd017
dmzhg <dmitry.zhgun@onegolabs.com>
2017-01-26 17:10:38 +0300

    Code format and description

--- src/main/java/com/onegolabs/wamanager/model/TempData.java
+++ src/main/java/com/onegolabs/wamanager/model/TempData.java
@@ -1,47 +1,48 @@
 package com.onegolabs.wamanager.model;
 
 /**
+ * For testing purposes
+ *
  * @author dmzhg
  */
 public class TempData {
 
-    private int code;
-    private String name;
+	private int code;
+	private String name;
+	private String description;
 
-    public String getDescription() {
-        return description;
-    }
+	public TempData(int code, String name, String description) {
+		this.code = code;
+		this.name = name;
+		this.description = description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    private String description;
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public int getCode() {
-        return code;
-    }
+	public int getCode() {
+		return code;
+	}
 
-    public void setCode(int code) {
-        this.code = code;
-    }
+	public void setCode(int code) {
+		this.code = code;
+	}
 
-    public TempData(int code, String name, String description) {
-        this.code = code;
-        this.name = name;
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Name : " + this.name + "\r\nCode: " + this.code + "\r\nDescription: " + this.description;
-    }
+	@Override
+	public String toString() {
+		return "Name : " + this.name + "\r\nCode: " + this.code + "\r\nDescription: " + this.description;
+	}
 }
