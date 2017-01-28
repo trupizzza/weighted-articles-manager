7a8d023a92f829637294cbab28ca31fdde8a893d
dmzhg <dmitry.zhgun@onegolabs.com>
2017-01-28 21:57:14 +0300

    added statmenet and connection

--- src/main/java/com/onegolabs/wamanager/dao/impl/ArticleDAOImpl.java
+++ src/main/java/com/onegolabs/wamanager/dao/impl/ArticleDAOImpl.java
@@ -3,12 +3,19 @@ package com.onegolabs.wamanager.dao.impl;
 import com.onegolabs.wamanager.dao.ArticleDAO;
 import com.onegolabs.wamanager.model.Article;
 
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.List;
 
 /**
  * @author dmzhg
  */
 public class ArticleDAOImpl implements ArticleDAO {
+
+	private Connection connection;
+	private Statement statement;
+
+
 	@Override
 	public List<Article> getAllArticles() {
 		return null;
