6e9ecc5ab1f59850f8874386ee5a32cb8fccb0e1
dmzhg <dmitry.zhgun@onegolabs.com>
2017-01-28 21:56:13 +0300

    Code moved for using in right classes

---
+++ src/main/java/com/onegolabs/wamanager/dbconnection/ConnectionFactory.java
@@ -0,0 +1,50 @@
+package com.onegolabs.wamanager.dbconnection;
+
+import com.onegolabs.wamanager.Configuration;
+import com.onegolabs.wamanager.exception.ConnectionCode;
+import com.onegolabs.wamanager.exception.InitializationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionFactory.class);
+
+	private static ConnectionFactory instance = new ConnectionFactory();
+
+	private Configuration config;
+
+	private ConnectionFactory(Configuration config) {
+		this.config = config;
+		try {
+			Class.forName(config.getProperty("connection.driver"));
+		} catch (ClassNotFoundException e) {
+			throw new InitializationException(ConnectionCode.NO_DRIVER_FOUND)
+					.set("url", config.getProperty("connection.driver"));
+		}
+	}
+
+	public static Connection getConnection() {
+		return instance.createConnection();
+	}
+
+	private Connection createConnection() {
+		Connection connection;
+		String url = null;
+		String user = null;
+		String password;
+		try {
+			url = config.getProperty("connection.url");
+			user = config.getProperty("connection.user");
+			password = config.getProperty("connection.password");
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			throw new InitializationException(ConnectionCode.NO_CONNECTION).set("url", url).set("user", user);
+		}
+		return connection;
+	}
+}
--- src/main/java/com/onegolabs/wamanager/dbconnection/ConnectionManager.java
+++ src/main/java/com/onegolabs/wamanager/dbconnection/ConnectionManager.java
@@ -1,14 +1,8 @@
 package com.onegolabs.wamanager.dbconnection;
 
-import com.onegolabs.wamanager.Configuration;
-import com.onegolabs.wamanager.exception.ConnectionCode;
-import com.onegolabs.wamanager.exception.InitializationException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
@@ -16,25 +10,33 @@ import java.sql.Statement;
  */
 public class ConnectionManager {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionManager.class);
-
-	public Connection connect(Configuration config) throws Exception {
-		LOGGER.info("Connecting to DB...");
-		Class clazz = Class.forName(config.getProperty("connection.driver").trim());
-		String address = config.getProperty("connection.url").trim();
-		String userName = config.getProperty("connection.user").trim();
-		String password = config.getProperty("connection.password").trim();
-
-		Connection connection = DriverManager.getConnection(address, userName, password);
-		Statement query = connection.createStatement();
-		ResultSet queryResult = query.executeQuery(" SELECT 1 FROM DUAL");
-		if (!queryResult.next()) {
-			throw new InitializationException(ConnectionCode.NO_CONNECTION).set("url", address);
+	public static void close(Connection connection) {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				/*log or print or ignore*/
+			}
 		}
-		return connection;
 	}
 
-//	public Connection getConnection() throws Exception {
-//		return connect();
-//	}
+	public static void close(Statement statement) {
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				/*log or print or ignore*/
+			}
+		}
+	}
+
+	public static void close(ResultSet resultSet) {
+		if (resultSet != null) {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				/*log or print or ignore*/
+			}
+		}
+	}
 }
